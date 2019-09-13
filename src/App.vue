d7ebcce2cd534ad737629cc7753972053da42806
Stephane Tombeur <stephane@tombeur.be>
2019-09-13 14:41:57 +0200

    bottom navigation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <template>
+
+<!-- <template>
+  <div id="jos">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/jobs">Jobs</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template> -->
+
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
@@ -16,9 +28,25 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
+    <v-bottom-navigation
+    v-model="bottomNav"
+  >
+    <v-btn value="home">
+       <router-link to="/"><span>Home</span><v-icon>mdi-history</v-icon></router-link> 
+    </v-btn>
+
+    <v-btn value="about">
+      <router-link to="/about"><span>About</span><v-icon>mdi-heart</v-icon></router-link> 
+    </v-btn>
+
+    <v-btn value="jobs">
+      <router-link to="/jobs"><span>Jobs</span><v-icon>mdi-map-marker</v-icon></router-link> 
+    </v-btn>
+  </v-bottom-navigation>
   </v-app>
+  
 </template>
 
 <script lang="ts">
